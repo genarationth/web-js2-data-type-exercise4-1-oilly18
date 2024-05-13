823a15f802246381ceccedbb0e5d5bf50182137c
oilly18 <oilly18@hotmail.com>
2024-05-13 14:28:14 +0700

    updatecommit

---
+++ objects.js
@@ -0,0 +1,14 @@
+const products = {
+    product_id:1,
+    product_name: "milk",
+    quantity_in_stock: 10,
+    unitprice: 35,
+    products_description:{
+        productbrand: "duchmill",
+        size_of_quantity: "180 ml",
+        tast_or_scent: "chocolate",
+        color:"brown",
+        additional_describtion: "delecious"
+    }
+}
+console.log(products);
